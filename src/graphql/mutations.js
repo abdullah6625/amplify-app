8811781528312dfc5d163b00a4a4d2dd74ad0565
abdullah6625 <abdullah6265@outlook.com>
2021-09-01 19:48:50 +0500

    add api

---
+++ src/graphql/mutations.js
@@ -0,0 +1,44 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+// export const createTodo = funct{/* GraphQL */ 
+//    mutation CreateTodo(name,id)
+//   {
+//     createTodo(input: $input, condition: $condition) {
+//       id
+//       name
+//       description
+//       createdAt
+//       updatedAt
+//     }
+//   }
+// }
+// ;
+export const updateTodo = /* GraphQL */ `
+  mutation UpdateTodo(
+    $input: UpdateTodoInput!
+    $condition: ModelTodoConditionInput
+  ) {
+    updateTodo(input: $input, condition: $condition) {
+      id
+      name
+      description
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const deleteTodo = /* GraphQL */ `
+  mutation DeleteTodo(
+    $input: DeleteTodoInput!
+    $condition: ModelTodoConditionInput
+  ) {
+    deleteTodo(input: $input, condition: $condition) {
+      id
+      name
+      description
+      createdAt
+      updatedAt
+    }
+  }
+`;
